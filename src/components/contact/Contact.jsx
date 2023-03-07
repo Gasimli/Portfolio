fe7bc55fe1b1a1f63cee53bcec253b745dffc477
Gasimli <fethi.qasimli@gmail.com>
2023-03-07 19:20:54 +0100

    Added EmailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,49 @@
 import React from "react";
 import "./contact.css";
+import { useRef } from "react";
+import emailjs from "emailjs-com"
 
 const Contact = () => {
-  return <section id="contact">
-    <h5>Get In Touch</h5>
-    <h2>Contact Me</h2>
-    <div className="container contact__container">
-      <form action="">
-        <input type="text" name="name" placeholder="Your Full Name" required />
-        <input type="text" name="email" placeholder="Your Email" required />
-        <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
-        <button type="submit" className="btn btn-primary">Send Messge</button>
-      </form>
-    </div>
-  </section>;
-};
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_urid3mr",
+        "template_2uc45j7",
+        form.current,
+        "X4wkUM6-nE0qBFrct"
+    )
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      {" "}
+      <h5>Get In Touch</h5> <h2>Contact Me</h2>{" "}
+      <div className="container contact__container">
+        {" "}
+        <form ref={form} onSubmit={sendEmail}>
+          {" "}
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />{" "}
+          <input type="text" name="email" placeholder="Your Email" required />{" "}
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>{" "}
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>{" "}
+        </form>{" "}
+      </div>{" "}
+    </section>
+  );
+};
 export default Contact;
